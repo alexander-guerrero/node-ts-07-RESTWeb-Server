bf6b2b314bfb450c9adeb12e711d8b154eeabfaa
Alexander Guerrero <alex.guelu@gmail.com>
2024-07-04 20:08:05 -0500

    192. Responder demás archivos

--- src/app.ts
+++ src/app.ts
@@ -17,18 +17,20 @@ const server = http.createServer((req, res) => {
         const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(htmlFile);
-    } else if (req.url === '/css/styles.css') {
-        const cssFile = fs.readFileSync('./public/css/styles.css');
+        return;
+    } 
+    
+    if (req.url?.endsWith('.css')) {
         res.writeHead(200, { 'Content-Type': 'text/css' })
-        res.end(cssFile);
-    } else if (req.url === '/js/app.js') {
-        const jsFile = fs.readFileSync('./public/js/app.js');
+    } else if (req.url?.endsWith('.js')) {
         res.writeHead(200, { 'Content-Type': 'application/javascript' })
-        res.end(jsFile);
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end();
     }
+
+    const responseContent = fs.readFileSync(`./public${req.url}`, 'utf-8');
+    res.end(responseContent);
 
 });
 
