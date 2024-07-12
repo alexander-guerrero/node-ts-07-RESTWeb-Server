084495c04dc65a7ec30e060a79f6fd38389c203f
Alexander Guerrero <alex.guelu@gmail.com>
2024-07-12 06:23:51 -0500

    195. Servir SPA con Router

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
 
@@ -10,6 +11,13 @@ export class Server {
 
         // * Public Folder
         this.app.use(express.static('public'));
+
+        this.app.get('*', (req, res) => {
+            // console.log(req.url);
+            // res.send('Hello world!!');
+            const indexPath = path.join(__dirname + '../../../public/index.html');
+            res.sendFile(indexPath);
+        });
         
         this.app.listen(3000, () => {
             console.log(`Server running on port ${3000}`);
