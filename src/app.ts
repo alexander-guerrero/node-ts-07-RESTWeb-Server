a598f1510a1bda4572c843f308a3403d64461e60
Alexander Guerrero <alex.guelu@gmail.com>
2024-07-14 19:55:01 -0500

    204. Rutas y Controladores

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/envs';
+import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
 
@@ -11,6 +12,7 @@ function main() {
     
     const server = new Server({
         port: envs.PORT,
+        routes: AppRoutes.routes,
         publicPath: envs.PUBLIC_PATH
     });
     server.start();
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     publicPath?: string;
 }
 
@@ -10,11 +11,13 @@ export class Server {
 
     private app = express();
     private readonly port: number;
+    private readonly routes: Router;
     private readonly publicPath: string;
 
     constructor(options: Options) {
-        const { port, publicPath = 'public' } = options;
+        const { port, routes, publicPath = 'public' } = options;
         this.port = port;
+        this.routes = routes;
         this.publicPath = publicPath;
     }
 
@@ -26,13 +29,7 @@ export class Server {
         this.app.use(express.static(this.publicPath));
 
         // * Routes
-        this.app.get('/api/todos', (req, res) => {
-            res.json([
-                { id: 1, text: 'Buy milk', createdAt: new Date() },
-                { id: 2, text: 'Buy bread', createdAt: null },
-                { id: 3, text: 'Buy butter', createdAt: new Date() }
-            ]);
-        })
+        this.app.use(this.routes);        
 
         // * Para router de SPAs (Single Page Application)
         this.app.get('*', (req, res) => {
