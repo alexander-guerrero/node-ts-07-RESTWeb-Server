bb15639950815563b9271fc7890f07ada1dad1c8
Alexander Guerrero <alex.guelu@gmail.com>
2024-07-21 15:37:15 -0500

    205. Todo Controller

---
+++ src/presentation/todos/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { TodosController } from './controller';
+
+export class TodosRoutes {
+
+    static get routes(): Router {
+
+        const router = Router();
+        const todoController = new TodosController();
+
+        router.get('/', 
+            // (req, res) => todoController.getTodos(req, res)
+            todoController.getTodos // En JavaScript se puede abreviar, son equivalentes
+        );
+
+        return router;
+
+    }
+
+}
