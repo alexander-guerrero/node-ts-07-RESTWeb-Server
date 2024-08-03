973214eec27ed3eb586c908dd906b421a018e0a1
Alexander Guerrero <alex.guelu@gmail.com>
2024-08-03 06:53:43 -0500

    230. TodoDatasource Implementation

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -11,4 +11,23 @@ export class TodoEntity {
         return !!this.completedAt; // Si tiene algún valor será true
     }
 
+    public static fromObject(object: { [key: string]: any }): TodoEntity {
+
+        const { id, text, completedAt } = object;
+
+        if (!id) throw 'id is required';
+        if (!text) throw 'text is required';
+
+        let newCompletedAt;
+        if (completedAt) {
+            newCompletedAt = new Date(completedAt);
+            if (isNaN(newCompletedAt.getTime())) {
+                throw 'completedAt is no a valid date';
+            }
+        }
+
+        return new TodoEntity(id, text, completedAt);
+
+    }
+
 }
