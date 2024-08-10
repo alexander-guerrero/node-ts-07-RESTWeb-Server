3ab1c67c22518c3f4dadadc572700bdc9d95d499
Alexander Guerrero <alex.guelu@gmail.com>
2024-08-10 17:26:41 -0500

    235. Express Best Practices + CleanArchitecture

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import compression from 'compression';
 import express, { Router } from 'express';
 import path from 'path';
 
@@ -26,6 +27,7 @@ export class Server {
         // * Middlewares
         this.app.use(express.json()); // raw
         this.app.use(express.urlencoded({extended: true})); // x-www-form-urlencoded
+        this.app.use(compression());
 
         // * Public Folder
         this.app.use(express.static(this.publicPath));
