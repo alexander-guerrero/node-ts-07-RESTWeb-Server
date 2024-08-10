cde5817d22042b5c138fd420e9c1202baf68e65d
Alexander Guerrero <alex.guelu@gmail.com>
2024-08-10 12:17:00 -0500

    234. Consumir los casos de uso

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
-import { TodoRepository } from '../../domain';
+import { CreateTodo, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateTodo } from '../../domain';
 
 export class TodosController {
 
@@ -9,50 +9,56 @@ export class TodosController {
         private readonly todoRepository: TodoRepository
     ) {}
 
-    public getTodos = async (req: Request, res: Response) => {
-        const todos = await this.todoRepository.getAll();
-        return res.json(todos);
+    public getTodos = (req: Request, res: Response) => {
+        new GetTodos(this.todoRepository)
+            .execute()
+            .then( todos => res.json(todos) )
+            .catch( error => res.status(400).json({error}) );
     }
 
-    public getTodoById = async (req: Request, res: Response) => {
+    public getTodoById = (req: Request, res: Response) => {
         const id = +req.params.id;
 
-        try {
-            const todo = await this.todoRepository.findById(id);
-            res.json(todo);
-        } catch (error) {
-            res.status(404).json({ error });
-        }
+        new GetTodo(this.todoRepository)
+            .execute(id)
+            .then( todo => res.json(todo) )
+            .catch( error => res.status(400).json({error}) );
 
         // Al no tener return se podría seguir ejecutando código luego del try...catch
         
     }
     
-    public createTodo = async (req: Request, res: Response) => {
+    public createTodo = (req: Request, res: Response) => {
         const [error, createTodoDto] = CreateTodoDto.create(req.body)
         if (error) return res.status(400).json({ error });
 
-        const newTodo = await this.todoRepository.create(createTodoDto!); // Con "!" se indica al compilador que "createTodoDto" no puede ser "null" o "undefined"
+        new CreateTodo(this.todoRepository)
+            .execute(createTodoDto!) // Con "!" se indica al compilador que "createTodoDto" no puede ser "null" o "undefined"
+            .then( newTodo => res.json(newTodo) )
+            .catch( error => res.status(400).json({error}) );
 
-        res.json(newTodo);
     }
 
-    public updateTodo = async (req: Request, res: Response) => {
+    public updateTodo = (req: Request, res: Response) => {
         const id = +req.params.id;
         const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id }); // El orden importa, primero el spread operator, y luego el id original del Path que reemplaza a cualquier otro id que se haya enviado en el Body
         if (error) return res.status(400).json({ error });
 
-        const updatedTodo = await this.todoRepository.updateById(updateTodoDto!)
+        new UpdateTodo(this.todoRepository)
+            .execute(updateTodoDto!)
+            .then( updatedTodo => res.json(updatedTodo) )
+            .catch( error => res.status(400).json({error}) );
 
-        res.json(updatedTodo);
     }
 
-    public deleteTodo = async (req: Request, res: Response) => {
+    public deleteTodo = (req: Request, res: Response) => {
         const id = +req.params.id;
 
-        const deletedTodo = await this.todoRepository.deleteById(id);
+        new DeleteTodo(this.todoRepository)
+            .execute(id)
+            .then( deletedTodo => res.json(deletedTodo) )
+            .catch( error => res.status(400).json({error}) );
 
-        res.json(deletedTodo);
     }
 
 }
