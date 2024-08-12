26ebd91c7b6e6150d7630f2f8c38e6489f13bc6d
Alexander Guerrero <alex.guelu@gmail.com>
2024-08-12 00:27:47 -0500

    245. Prueba - GET - api/todos

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { testServer } from '../../test-server';
+import { prisma } from '../../../src/data/postgres';
 
 describe('Testing routes.ts', () => {
 
@@ -10,14 +11,28 @@ describe('Testing routes.ts', () => {
     afterAll(() => {
         testServer.close();
     });
+
+    const todo1 = { text: 'Hello world! 1' };
+    const todo2 = { text: 'Hello world! 2' };
     
     test('should return TODOs api/todos', async () => {
+
+        await prisma.todo.deleteMany();
+        await prisma.todo.createMany({
+            data: [todo1, todo2]
+        });
         
-        const response = await request(testServer.app)
+        const { body } = await request(testServer.app)
             .get('/api/todos')
             .expect(200);
         
-        console.log(response.body);
+        // console.log(body);
+
+        expect(body).toBeInstanceOf(Array);
+        expect(body.length).toBe(2);
+        expect(body[0].text).toBe(todo1.text);
+        expect(body[1].text).toBe(todo2.text);
+        expect(body[0].completedAt).toBeNull();
 
     });
 
