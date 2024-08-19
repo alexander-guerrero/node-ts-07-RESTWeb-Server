0f076329e5f7d176802c3239795e19169736bf2c
Alexander Guerrero <alex.guelu@gmail.com>
2024-08-18 20:43:23 -0500

    246. Prueba - GET - api/todos/:id

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -12,12 +12,15 @@ describe('Testing routes.ts', () => {
         testServer.close();
     });
 
+    beforeEach(async () => {
+        await prisma.todo.deleteMany();
+    });
+
     const todo1 = { text: 'Hello world! 1' };
     const todo2 = { text: 'Hello world! 2' };
     
     test('should return TODOs api/todos', async () => {
 
-        await prisma.todo.deleteMany();
         await prisma.todo.createMany({
             data: [todo1, todo2]
         });
@@ -33,6 +36,27 @@ describe('Testing routes.ts', () => {
         expect(body[0].text).toBe(todo1.text);
         expect(body[1].text).toBe(todo2.text);
         expect(body[0].completedAt).toBeNull();
+
+    });
+    
+    test('should return a TODO api/todos/:id', async () => {
+
+        // 1. Arrange
+        const newTodo = await prisma.todo.create({
+            data: todo1
+        });
+
+        // 2. Act
+        const { body } = await request(testServer.app)
+            .get(`/api/todos/${newTodo.id}`)
+            .expect(200)
+
+        // 3. Assert
+        expect(body).toEqual({
+            id: newTodo.id,
+            text: newTodo.text,
+            completedAt: newTodo.completedAt
+        });
 
     });
 
